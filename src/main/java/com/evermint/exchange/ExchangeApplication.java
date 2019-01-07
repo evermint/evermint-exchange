5db48be27e9f247a91e5f956ca9d2ac0e31229d2
Evermint Design Team <designteam@evermint.com>
2019-01-07 12:46:04 -0700

    Java-sprintboot implementation, inital commit.

---
+++ src/main/java/com/evermint/exchange/ExchangeApplication.java
@@ -0,0 +1,14 @@
+package com.evermint.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeApplication.class, args);
+	}
+
+}
+
